220964f619fd35cd3abb1b531cbf0ac9f4638f46
dgparker <dylan@codecave.app>
2023-09-19 22:28:27 -0500

    publish and dns try 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://kai-docs.dreamstax.io',
 	integrations: [
 		starlight({
 			title: 'Kai',
